124566742927bddd29168aa838e17951962c3c9b
Julia Ryzhova <16-j.lazy@bk.ru>
2023-09-18 08:22:36 +0300

    Fix home_work 4

--- functional_programming/main.py
+++ functional_programming/main.py
@@ -24,12 +24,9 @@ def pearson_correlation(numbers_1: List[float], numbers_2: List[float]) -> float
     mean_2 = sum(numbers_2) / n
 
     # Вычисление ковариации и дисперсии для массивов numbers_1 и numbers_2
-    covariance = sum((numbers_1[i] - mean_1) *
-                     (numbers_2[i] - mean_2) for i in range(n))
-    variance_numbers_1 = sum((numbers_1i - mean_1)
-                             ** 2 for numbers_1i in numbers_1)
-    variance_numbers_2 = sum((numbers_2i - mean_2)
-                             ** 2 for numbers_2i in numbers_2)
+    covariance = sum((numbers_1[i] - mean_1) * (numbers_2[i] - mean_2) for i in range(n))
+    variance_numbers_1 = sum((x - mean_1) ** 2 for x in numbers_1)
+    variance_numbers_2 = sum((y - mean_2) ** 2 for y in numbers_2)
 
     # Расчет корреляции Пирсона
     correlation = covariance / \
@@ -38,8 +35,8 @@ def pearson_correlation(numbers_1: List[float], numbers_2: List[float]) -> float
     return round(correlation)
 
 
-x = [1, 2, 3, 4, 5]
-y = [5, 4, 3, 2, 1]
+num_1 = [1, 2, 3, 4, 5]
+num_2 = [5, 4, 3, 2, 1]
 
-correlation = pearson_correlation(x, y)
+correlation = pearson_correlation(num_1, num_2)
 print(f"Корреляция Пирсона: {correlation}")
